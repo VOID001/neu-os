3a0402a01517f75eec905eb8a4e67aab6563a99e
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2017-03-13 15:42:55 +0800

    Lesson 2 加载软盘中的内容到内存，并跳转执行

--- Makefile
+++ Makefile
@@ -7,16 +7,30 @@ LDFLAGS	+= -Ttext 0
 
 .PHONY=clean run all
 
-all: bootsect
+all: bootimg
 
 bootsect: bootsect.s
-	@$(AS) -o bootsect.o bootsect.s
+	@$(AS) -n -g -o bootsect.o bootsect.s
 	@$(LD) $(LDFLAGS) -o bootsect bootsect.o
 	@cp -f bootsect bootsect.sym
 	@$(OBJCOPY) -R .pdr -R .comment -R.note -S -O binary bootsect
 
-run: bootsect
-	$(QEMU) -boot a -fda bootsect
+demo: demo.s
+	@$(AS) -n -g -o demo.o demo.s
+	@$(LD) $(LDFLAGS) -o demo demo.o
+	@cp -f demo demo.sym
+	@$(OBJCOPY) -R .pdr -R .comment -R.note -S -O binary demo
+
+bootimg: demo bootsect
+	@dd if=bootsect of=bootimg bs=512 count=1
+	@dd if=demo of=bootimg bs=512 count=4 seek=1
+	@echo "Build bootimg done"
+
+run: bootimg
+	$(QEMU) -boot a -fda bootimg
+
+run_debug:
+	$(QEMU) -boot a -fda bootimg -S -s
 
 clean:
-	@rm -f bootsect bootsect.o bootsect.sym
+	@rm -f bootsect bootsect.o bootsect.sym	demo demo.sym bootimg a.out
--- bootsect.s
+++ bootsect.s
@@ -1,12 +1,13 @@
 #################################################################
 #                                                               #
-#  Lesson 1: 创建一个"Hello World"引导扇区                      #
-#  Goal: 了解操作系统引导的过程，并且通过使用实模式下的汇编     #
-#  在屏幕上输出一行字符串并进入到死循环                         #
+#  Lesson 2: 加载软盘中的内容到内存，并执行相应代码				#
+#  Goal: 了解 int 0x13 的使用方法，以及了解如何使用ljmp切换     #
+#  CS 和IP														#
 #                                                               #
 #################################################################
 
 	.code16        # 指定语法为 十六位汇编
+	.equ SYSSIZE, 0x3000 # System Size in Clicks
 
 # rewrite with AT&T syntax by falcon <wuzhangjin@gmail.com> at 081012
 # Modified by VOID001<zhangjianqiu13@gmail.com> at 2017 03 05
@@ -14,11 +15,21 @@
 
 	#.global _start, begtext, begdata, begbss, endtext, enddata, endbss
 	.global _start # 程序开始处
+	.global begtext, begdata, begbss, endtext, enddata, endbss
+	.text
+	begtext:
+	.data
+	begdata:
+	.bss
+	begbss:
 	.text
 
 	.equ BOOTSEG, 0x07c0		# 当此扇区被BIOS识别为启动扇区装载到内存中时，装载到0x07c0段处
+	.equ INITSEG, 0x9000
+	.equ DEMOSEG, 0x1000		# 要调用的函数在的段地址
 								# 此时我们处于实模式(REAL MODE)中，对内存的寻址方式为
 								# (段地址 << 4 + 偏移量) 可以寻址的线性空间为 20 位
+	.equ ROOTDEV, 0x301			# 指定/dev/fda为系统镜像所在的设备
 
 	ljmp    $BOOTSEG, $_start   # 修改cs寄存器为BOOTSEG, 并跳转到_start处执行我们的代码
 
@@ -30,14 +41,30 @@ _start:
 	int	$0x10
 	
 	mov	$20, %cx			# Set the output length
-	mov	$0x0007, %bx		# page 0, attribute 7 (normal) 设置必要的属性
-	#lea	msg1, %bp
+	mov	$0x0007, %bx		# page 0, color = 0x07 (from wikipedia https://en.wikipedia.org/wiki/INT_10H)
 	mov     $msg1, %bp
 	mov	$0x1301, %ax		# write string, move cursor
 	int	$0x10				# 使用这个中断0x10的时候，输出的内容是从 ES:BP 中取得的，因而要设置好 ES 和 BP
 
-loop_forever:
-	jmp loop_forever
+load_demo:
+	# 这里我们需要将软盘中的内容加载到内存中，并且跳转到相应地址执行代码
+	mov $0x0000, %dx		# 选择磁盘号0，磁头号0进行读取
+	mov $0x0002, %cx		# 从二号扇区，0轨道开始读(注意扇区是从1开始编号的)
+	mov $DEMOSEG, %ax		# ES:BX 指向装载目的地址
+	mov %ax, %es
+	mov $0x0200, %bx		
+	mov $02, %ah			# Service 2: Read Disk Sectors
+	mov $4, %al				# 读取的扇区数
+	int $0x13				# 调用BIOS中断读取
+	jnc demo_load_ok		# 没有异常，加载成功
+	jmp load_demo			# 并一直重试，直到加载成功
+
+demo_load_ok:				# Here will jump to where the demo program is
+	mov $DEMOSEG, %ax
+	#mov %ax, %cs			# This is awful!! Do not change CS alone!! It will result in GDB cannot debug the code
+							# And, of course the code will not work
+	mov %ax, %ds
+	ljmp $0x1020, $0		# jump to where the demo program exists
 
 sectors:
 	.word 0
