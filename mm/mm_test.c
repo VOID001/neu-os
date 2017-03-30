8a11c89d7420612a8fa5633ea4224f7c370f2675
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2017-03-30 22:17:55 +0800

    Lesson 7: Paging Experiment
    1. Put a page at specific address
    2. Modify page attribute
    3. Print page metainfo

--- mm/mm_test.c
+++ mm/mm_test.c
@@ -64,17 +64,66 @@ void disable_linear(unsigned long addr) {
     return ;
 }
 
+void mm_read_only(unsigned long addr) {
+    unsigned long *pte = linear_to_pte(addr);
+    printk("Before: 0x%x\n", *pte);
+    *pte = *pte & 0xfffffffd;
+    printk("After: 0x%x\n", *pte);
+    invalidate();
+    return ;
+}
+
+void mm_print_pageinfo(unsigned long addr) {
+    unsigned long *pte = linear_to_pte(addr);
+    printk("Linear addr: 0x%x, PTE addr = 0x%x. Flags[ ", addr, pte);
+    if(*pte & 0x1) printk("P ");
+    if(*pte & 0x2) printk("R/W ");
+    else printk("RO ");
+    if(*pte & 0x4) printk("U/S ");
+    else printk("S ");
+    printk("]\n");
+    printk("Phyaddr = %x\n", (*pte & 0xfffff000));
+}
+
 
 int mmtest_main(void) {
     int i = 0;
     printk("Running Memory function tests\n");
-    printk("1. Make Linear Address 0x200000 unavailable\n");
-    disable_linear(0x200000);
-    char *x = 0x200000;
-    *x = 1;
-    // This will cause page fault =w=
-    // Computer will reboot again and again
-    i = 0;
+    printk("1. Make Linear Address 0xdad233 unavailable\n");
 
-    while(1);
+    disable_linear(0xdad233);
+
+    // Now don't modify the code above
+    // make the linear address AT 0xdad233 accessible
+    // and store 0x23333333 at 3MB physical memory
+    // (Hint: add a table entry map to physical address 3MB)
+    //
+    printk("2. Put page(0x300000) at linear address 0xdad233\n");
+    put_page(0x300000, 0xdad233);
+
+    unsigned long *x = 0xdad233;
+    *x = 0x23333333;
+    printk("X = %x\n", *x);
+
+    // Then make the linear address 0xdad233 Read Only
+    
+    printk("3. Make 0xdad233 READ ONLY\n");
+    mm_read_only(0xdad233);
+    x = 0xdad233;
+    // DO not modify this code
+    // Here will disable WP bit (temporarily)
+    asm volatile("mov %%cr0, %%eax\n\t"
+            "orl $0x00010000, %%eax\n\t"
+            "mov %%eax, %%cr0\n\t"
+            ::);
+    // Please write a GCC Extended assembly here
+    // to modify the content at linear address 0xdad233
+    // (This will trigger a page write protect fault), uncomment lines below to test
+    //asm volatile("mov %%eax, (%%edi)"
+    //        ::"D" (x), "a" (0x233111));
+
+    printk("4. Print the page info of 0xdad233 in human readable mode\n");
+    mm_print_pageinfo(0xdad233);
+
+    // Please implement
 }
